432cbfaa184cb57d5bb8b8bee50bcbbc471c352c
Franco Calegari <francocalegari55@gmail.com>
2025-12-17 17:56:36 -0300

    upgrades

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
 	const { username, password } = req.body;
-	if (username === "admin" && password === "admin") {
+	if (username === "admin" && password === "admin123") {
 		req.session.user = { username };
 		res.status(200).json({ success: true });
 	} else {
