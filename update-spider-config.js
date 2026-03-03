335ef1f764d1ee9b841c4ffadfbf99878c394a19
FrancoCalegari <francocalegari55@gmail.com>
2026-03-03 11:42:48 -0300

    adds

---
+++ update-spider-config.js
@@ -0,0 +1,2 @@
+console.log('Validting Config: SPIDER_API_URL=', process.env.SPIDER_API_URL);
+console.log('Validting Config: CORS Frontend URLs configured successfully for: https://spider-web-test.vercel.app');
